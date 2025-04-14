67537374549f4beca539fcc3ec046333cc3e67fa
Roman <romanpathway@gmail.com>
2025-04-14 14:25:27 +0300

    Lab 6

--- Auth_Pr/server.js
+++ Auth_Pr/server.js
@@ -126,7 +126,7 @@ app.post('/reset-password', (req, res) => {
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
-                },
+                }
             });
 
             const mailOptions = {
